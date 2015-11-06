38053262a9f2eade65aa84e7ce4875f09e9e7999
Geoffroy Bergeron <Helix@Geoffroys-MacBook-Air.local>
2015-11-06 16:39:58 -0500

    Gulp first setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,23 @@
 var gulp = require('gulp');
+var shell = require('gulp-shell');
+var postcss = require('gulp-postcss');
+var livereload = require('gulp-livereload');
 
-gulp.task('default', function() {
+gulp.task('default', ['css','start','watch']);
 
+gulp.task('start', shell.task(['./bin/www']));
+
+gulp.task('css', function() {
+	var processors = [
+		
+	]
+	
+	return gulp.src('/source/styles/*.css')
+		.pipe(postcss(processors))
+		.pipe(gulp.dest('public/styles'));
+});
+
+gulp.task('watch', function(){
+	livereload.listen({start: true});
+	gulp.watch('/source/styles/*.css',['css']);
 });
