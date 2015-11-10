6f79d96405d1db5b4354347a4d6125722f995371
gaudreaujacksoncharles <gaudreaujacksoncharles@gmail.com>
2015-11-10 06:02:31 -0500

    Added react lib

---
+++ source/js/components/topbar/topbar.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+//var AppActions = require('actions');
+//var AppStore = require('actions');
+
+// Topbar component
+var Topbar = React.createClass({
+  render:function() {
+    return (
+      <div className="topbar">
+        Topbar section
+      </div>
+    )
+  }
+});
