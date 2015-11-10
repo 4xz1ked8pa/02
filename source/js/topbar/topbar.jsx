b96e08eb00d7e536031160c9f31d4d56e2c943e7
gaudreaujacksoncharles <gaudreaujacksoncharles@gmail.com>
2015-11-10 05:55:11 -0500

    Component test

--- source/js/topbar/topbar.jsx
+++ source/js/topbar/topbar.jsx
@@ -1,6 +1,6 @@
-var React = require('react');
-var AppActions = require('AppActions');
-var AppStore = require('AppStore');
+//var React = require('react');
+//var AppActions = require('actions');
+//var AppStore = require('actions');
 
 // Topbar component
 var Topbar = React.createClass({
