bdd8f346b917698082f61bdef3603d52811f5818
Geoffroy Bergeron <Helix@Geoffroys-MacBook-Air.local>
2015-11-12 02:44:36 -0500

    merge

--- source/js/components/root.jsx
+++ source/js/components/root.jsx
@@ -1,13 +1,15 @@
 // Root component of UI
 var React = require('react');
-//var ToBar = require('./topbar/topbar.jsx');
+var ToBar = require('./topbar/topbar.jsx');
 //var ChatBar = require('./chatbar/chatbar.jsx');
 //var Sidebar = require('./sidebar/sidebar.jsx');
 //var Main = require('./main/main.jsx');
 
 var Root = React.createClass({
 	render: function() {
-		return <div></div>;
+		return <div>
+			<TopBar />
+		</div>;
 	}
 });
 
