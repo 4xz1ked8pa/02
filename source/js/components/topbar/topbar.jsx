25ec1159a05d0472a893e320709b2cdb6e20a8cb
gaudreaujacksoncharles <gaudreaujacksoncharles@gmail.com>
2015-11-11 20:14:30 -0500

    Merge

---
+++ source/js/components/topbar/topbar.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+// Topbar component
+var Topbar = React.createClass({
+  render:function() {
+    return (
+      <div className="topbar">
+        Topbar section test
+      </div>
+    )
+  }
+});
